5cd7d0d608aca199883363ef38821db4049016ec
Abiodun Alao <abiodundev@gmail.com>
2023-07-10 13:44:20 +0100

    chore/use kafka to send data to queue

--- src/controllers/sendMail.controller.ts
+++ src/controllers/sendMail.controller.ts
@@ -1,19 +1,38 @@
 import MailService from '../services/mail.services';
+import { producer } from '../config/kafka.config';
+import { EmailOptions } from '../types/sendMail.types';
 
 
 export default class SendMailController {
     private mailService: MailService;
-    
+
     constructor() {
         this.mailService = new MailService();
     }
 
     async sendMail(req, res) {
         try {
-            const { destinationEmail, sourceEmail, body, subject } = req.body;
+            const { destinationEmail, sourceEmail, body, subject } = req.body as EmailOptions;
+
             const data = { destinationEmail, sourceEmail, body, subject };
-            await this.mailService.sendMail(data);
+
+            // connect to kafka to producer
+            await producer.connect();
+
+            await Promise.all([
+                producer.send({
+                    topic: String(process.env.KAFKA_TOPIC),
+                    messages: [{ 
+                        key: process.env.KAFKA_MESSAGE_KEY, 
+                        value: JSON.stringify({...req.body, action: 'SEND_MAIL'}) 
+                    }]
+                }),
+    
+                this.mailService.sendMail(data)
+            ])
+
             return res.status(200).json({ message: 'Email Sent Successfully' });
+
         } catch (error) {
             console.log(error);
             return res.status(500).json({ message: 'Internal Server Error' });
